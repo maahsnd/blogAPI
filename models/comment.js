534b79767a23db34e3bf3a2e50f21a35067fc437
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-01 09:51:04 -0700

    Create comment model

---
+++ models/comment.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+const { text } = require('express');
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  text: { type: text, required: true, maxLength: 500 },
+  date: { type: Date, default: Date.now },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  post: { type: Schema.Types.ObjectId, ref: 'Post', required: true }
+});
+
+CommentSchema.virtual('formatted_date').get(function () {
+  return DateTime.fromJSDate(this.date).toISODate();
+});
+
+CommentSchema.virtual('url').get(function () {
+  return `/blog/posts/:postId/${this._id}`;
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);

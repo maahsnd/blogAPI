53ff2a4b31e9877c7f247132b001896bb9e0db62
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-03 15:36:14 -0700

    Remove logs, comments

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,23 +1,22 @@
 const Post = require('../models/post');
 const User = require('../models/user');
+const Comments = require('../models/comment');
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 
 //Display list of all posts
 exports.posts_get = asyncHandler(async (req, res, next) => {
-  console.log('7');
   const allPosts = await Post.find({ published: true })
     .populate('user')
-    /*  .populate('comments')
+    .populate('comments')
     .populate({
       path: 'comments',
       populate: {
         path: 'user',
         populate: { path: 'user_name' }
       }
-    }) */
+    })
     .sort({ date: -1 })
     .exec();
-  console.log('posts:' + allPosts[0]);
   res.render('posts_list', { title: 'All posts', posts: allPosts });
 });

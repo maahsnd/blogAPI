48ee8ae7da6eada61cb4c13a12bf738fa203b203
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-04 09:51:28 -0700

    Add user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const authenticationController = require('../controllers/authentication-controller');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a sign up/log in page, or with log out');
 });
+
+router.get('/sign-up', authenticationController.sign_up_get);
+
+router.post('/sign-up', authenticationController.sign_up_post);
+
+router.get('/log-in', authenticationController.log_in_get);
+
+router.post('/log-in', authenticationController.log_in_post);
+
+router.post('/log-out', authenticationController.logout_get);
 
 module.exports = router;

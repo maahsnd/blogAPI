aca6906f5d8e034aa08eac9028047db7a2e88bad
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-05 09:49:44 -0700

    Protect routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/post-controller');
-const verifyToken = require('../verifyToken').verifyToken;
 const passport = require('passport');
 
 /* GET home page. */
 router.get('/', postController.all_blogposts_get);
 
 /* GET new blogpost */
-router.get('/new-post', verifyToken, postController.new_blogpost_get);
-
+router.get(
+  '/new-post',
+  passport.authenticate('jwt', { session: false }),
+  postController.new_blogpost_get
+);
 /* POST new blogpost */
-router.post('/new-post', postController.new_blogpost_post);
+router.post(
+  '/new-post',
+  passport.authenticate('jwt', { session: false }),
+  postController.new_blogpost_post
+);
 
 /* GET edit blogpost */
 router.get(
@@ -21,7 +27,11 @@ router.get(
 );
 
 /* POST edit blogpost */
-router.post('/edit-post', postController.blogpost_edit_post);
+router.post(
+  '/edit-post',
+  passport.authenticate('jwt', { session: false }),
+  postController.blogpost_edit_post
+);
 
 /* GET individual post */
 router.get('/:id', postController.blogpost_get);

0b0af5ceac97bb3bcca81a5b04385e1dad2ab6ce
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-11 10:42:24 -0700

    Create comment router

---
+++ routes/comments.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/comment-controller');
+const passport = require('passport');
+
+router.post(
+  '/new-comment',
+  passport.authenticate('jwt', { session: false }),
+  commentController.new_comment
+);
+
+router.get('/:id', commentController.get_comment);
+
+router.post(
+  '/:id/edit',
+  passport.authenticate(
+    'jwt',
+    { session: false },
+    commentController.edit_comment
+  )
+);
+
+router.post(
+  '/:id/delete',
+  passport.authenticate('jwt', { session: false }),
+  commentController.delete_comment
+);
+module.exports = router;
